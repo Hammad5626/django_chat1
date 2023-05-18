80afb609f3cb34e956ca370f5fd6fe1fc2bb9a01
Hamad Badar <Hamadbadar5626@hotmail.com>
2023-05-18 10:47:34 +0500

    FM

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -3,9 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import room.routing
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
-
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
--- room/consumers.py
+++ room/consumers.py
@@ -1,21 +1,17 @@
 import json
-
 from django.contrib.auth.models import User
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
-
 from .models import Room, Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-
         await self.accept()
 
     async def disconnect(self):
@@ -24,17 +20,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
         data = json.loads(text_data)
         print(data)
         message = data['message']
         username = data['username']
         room = data['room']
-
         await self.save_message(username, room, message)
-
-        # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -44,12 +36,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
         username = event['username']
-
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message,
             'username': username
@@ -59,5 +48,4 @@ class ChatConsumer(AsyncWebsocketConsumer):
     def save_message(self, username, room, message):
         user = User.objects.get(username=username)
         room = Room.objects.get(slug=room)
-
         Message.objects.create(user=user, room=room, content=message)
